4a4fa9e3bdac73e68a0a515f4cb35e9ffda70254
Aarew <warakorn.sup@gmail.com>
2024-01-30 18:21:57 +0700

    Aarew_Accorditionfixed_withpath

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import ActivityList from "./Pages/Activity.jsx/ActivityList";
+import TutorialPage from "./Pages/Tutorial/Tutorial-page";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<ActivityList />} />
+            <Route path="/" element={<ActivityList />} />
+            <Route path="/videoTutorial" element={<TutorialPage />} />
         </Routes>
       </BrowserRouter>
     </div>
