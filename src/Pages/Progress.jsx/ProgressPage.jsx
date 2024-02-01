01ae9055ef72a6922c3127d3093054d3e366b79e
fruityty <danuphat.lim@gmail.com>
2024-02-02 00:02:28 +0700

    add date()

--- src/Pages/Progress.jsx/ProgressPage.jsx
+++ src/Pages/Progress.jsx/ProgressPage.jsx
@@ -4,12 +4,18 @@ import ProgressChart from "./ProgressChart";
 import ProgressActivityCard from "./ProgressActivityCard";
 
 function ProgressPage() {
+  const date = () => {
+    const currentDate = new Date();
+    const formattedDate = currentDate.toDateString();
+
+    return <p className="activityCard-date">{formattedDate}</p>;
+  };
   return (
     <Layout>
       <div className="progress">
         <div className='head'>
           <h2>Progress</h2>
-          <p>Dec, 5 2023</p>
+          {date()}
         </div>
         <ProgressChart />
         <ProgressActivityCard />
