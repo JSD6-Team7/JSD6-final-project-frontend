95f6cac7b9f62c712b9f1c8628ae477250b03ea0
Aarew <warakorn.sup@gmail.com>
2024-02-15 15:37:31 +0700

    Aarew_Axoisinitiate

--- src/Pages/Tutorial/Tutorial-page.jsx
+++ src/Pages/Tutorial/Tutorial-page.jsx
@@ -2,6 +2,8 @@ import Layout from "../Layout.jsx";
 import "./TutorialCSS.css";
 import Accordian from "../Tutorial/Accordioncard.jsx";
 import Addvideo from "../Tutorial/Addvideo.jsx";
+import axios from "axios";
+import dayjs from "dayjs";
 import React, { useEffect, useState } from 'react';
 import { v4 as uuid } from "uuid";
 
@@ -9,13 +11,11 @@ import { v4 as uuid } from "uuid";
 
 const data = [
   {
-      id: uuid(),
       label: "What a cool label",
       video: "https://www.youtube.com/embed/AnYl6Nk9GOA",
       descriptions: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dapibus, felis eget tincidunt sollicitudin, tortor lacus varius odio, vitae elementum dolor velit ut sapien. Nulla facilisi. Phasellus luctus diam in tortor laoreet, quis laoreet dui sagittis. Fusce in arcu vel ipsum pharetra feugiat non vitae dolor. Suspendisse potenti. Praesent accumsan sagittis massa, vitae commodo erat facilisis et. Morbi mauris risus, semper at imperdiet et, tincidunt eget quam. Praesent commodo eros ut ipsum condimentum, a pulvinar tortor sollicitudin. Mauris ultricies eros neque, in finibus erat consequat laoreet. Nullam ultrices mollis nulla, non dictum ex cursus mattis. Praesent tincidunt sodales tincidunt. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc ultrices tellus vel neque accumsan tempus id quis augue. Donec eu placerat tellus. Sed justo velit, sagittis eu mi elementum, tempor lacinia leo. Suspendisse faucibus augue vitae semper congue. Sed viverra, dui sit amet viverra aliquet, metus ipsum faucibus urna, vel porttitor felis neque vel tortor. Nulla maximus sit amet elit eget posuere. Pellentesque iaculis nisi orci, non dictum justo finibus vitae. Maecenas nisi ipsum, aliquet gravida pellentesque vitae, sodales id purus. Vestibulum imperdiet lorem sit amet urna tempus consectetur. Sed non nibh nisi. Nunc tempor sapien neque, eu malesuada risus condimentum sit amet.",
   },
   {
-      id: uuid(),
       label: "What a cool label",
       video: "https://www.youtube.com/embed/zpJXXn_-XuY",
       descriptions: "",
@@ -26,9 +26,14 @@ const newData = data.map((item, index) => ({ ...item, index }));
 const items = newData;
 
 function TutorialPage() {
-  const [accordionItems, setAccordionItems] = useState(items);
+  const [accordionItems, setAccordionItems] = useState(null);
   const [isFormOpen, setIsFormOpen] = useState(false);
-  const [formDisplay, setFormDisplay] = useState();
+  const [formDisplay, setFormDisplay] = useState(null);
+
+  const userString = localStorage.getItem("user");
+  const userObject = JSON.parse(userString);
+  const user_id = userObject.user_id;
+  const token = userObject.token;
 
   useEffect(() => {
     if (items) {
@@ -42,36 +47,117 @@ function TutorialPage() {
   }, [items])
 
   useEffect(() => {
+    getActivityInfo();
+  }, []);
+
+  useEffect(() => {
     console.log(formDisplay);
     if (formDisplay) {
       setIsFormOpen(true);
     }
   }, [formDisplay]);
 
-  const createItem = (item) => {
-    setAccordionItems((prev) => {
-      return [...prev, { ...item, id: uuid(), index: prev.length}];
-    });
-  };
-  const deleteItem = (id) => {
-    setAccordionItems((prev) => {
-      return prev.filter((each) => {
-        return each.id !== id;
+  const getActivityInfo = () => {
+    axios
+      .get(`http://localhost:3000/activityInfo/${user_id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setActivityItems((prev) => {
+          return response.data.map((each) => {
+            return { ...each, date: dayjs(each.date) };
+          });
+        });
+      })
+      .catch((error) => {
+        console.error("Error: ", error);
       });
-    });
   };
+
+  const createItem = (item) => {
+    console.log(item);
+    const newActivity = { ...item, user_id };
+    console.log(newActivity.date);
+    axios
+      .post("http://localhost:3000/activityInfo", newActivity, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      .then((response) => {
+        if (response.status === 201) {
+          getActivityInfo();
+        }
+      })
+      .catch((error) => {
+        console.error("Failed to add data", error);
+      });
+  };
+
+  const deleteItem = (id) => {
+    axios
+      .delete(`http://localhost:3000/activityInfo/${id}`, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          getActivityInfo();
+        } else {
+          console.log(`Response from API : ${response.json}`);
+        }
+      })
+      .catch((error) => {
+        console.error("Failed to delete", error);
+      });
+  };
+
   const updateItem = (item) => {
     console.log(item);
-    setAccordionItems((prev) => {
-      return prev.map((each) => {
-        if (each.id === item.id) {
-          return item;
+    axios
+      .put("http://localhost:3000/activityInfo", item, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          getActivityInfo();
         } else {
-          return each;
+          console.error("Failed to update");
         }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
       });
-    });
   };
+  // useEffect(() => {
+  //   console.log(formDisplay);
+  //   if (formDisplay) {
+  //     setIsFormOpen(true);
+  //   }
+  // }, [formDisplay]);
+
+  // const createItem = (item) => {
+  //   setAccordionItems((prev) => {
+  //     return [...prev, { ...item, id: uuid(), index: prev.length}];
+  //   });
+  // };
+  // const deleteItem = (id) => {
+  //   setAccordionItems((prev) => {
+  //     return prev.filter((each) => {
+  //       return each.id !== id;
+  //     });
+  //   });
+  // };
+  // const updateItem = (item) => {
+  //   console.log(item);
+  //   setAccordionItems((prev) => {
+  //     return prev.map((each) => {
+  //       if (each.id === item.id) {
+  //         return item;
+  //       } else {
+  //         return each;
+  //       }
+  //     });
+  //   });
+  // };
 
 
   return (
