d0bd6ea97f19dd9f7c263683c3d91b4ce538b18f
fon <anatsaya.lee@gmail.com>
2024-02-16 09:49:49 +0700

    fix bug

--- src/Pages/Tutorial/Accordioncard.jsx
+++ src/Pages/Tutorial/Accordioncard.jsx
@@ -1,92 +1,106 @@
-import React, { useEffect, useState } from 'react';
-import { PlusCircleFilled, MinusCircleFilled } from '@ant-design/icons'
+import React, { useEffect, useState } from "react";
+import { PlusCircleFilled, MinusCircleFilled } from "@ant-design/icons";
 
-
-const Accordion = ( { items, keepOthersOpen } ) => {
-
-  const [accordionItems, setAccordionItems ] = useState(null)
+const Accordion = ({ items, keepOthersOpen }) => {
+  const [accordionItems, setAccordionItems] = useState(null);
 
   useEffect(() => {
     if (items) {
       setAccordionItems([
-        ...items.map(item => ({
+        ...items.map((item) => ({
           ...item,
-          toggled: false
-        }))
-      ])
+          toggled: false,
+        })),
+      ]);
     }
-  }, [items])
+  }, [items]);
 
   function handleAccordionToggle(clickedItem) {
-    setAccordionItems([
-      ...accordionItems.map((item) => {
-        let toggled = item.toggled
+    setAccordionItems(
+      accordionItems.map((item) => {
+        let toggled = item.toggled;
 
         if (clickedItem.id === item.id) {
-          toggled = !item.toggled
+          toggled = !item.toggled;
         } else if (!keepOthersOpen) {
-          toggled = false
+          toggled = false;
         }
 
-        return  {
+        return {
           ...item,
-          toggled
-        }
+          toggled,
+        };
       })
-    ])
+    );
   }
 
-  return( 
-    <div className='accordion-parent'>
-      <div className='headunderline'>
-        <h2 className='headunderline-item'>Video Tutorial</h2>
+  return (
+    <div className="accordion-parent">
+      <div className="headunderline">
+        <h2 className="headunderline-item">Video Tutorial</h2>
         <div>
-          <button className='headunderline-button'>+ Video</button>
+          <button className="headunderline-button">+ Video</button>
         </div>
       </div>
       {accordionItems?.map((listItem, key) => {
         return (
-          
-          <div class={`accordion ${listItem.toggled ? 'toggled' : ''}`} key={key}>
-            <button className='toggle' onClick={() => handleAccordionToggle(listItem)}>
-              <div className='labelcard'>
-                <div className='number-highlighted'>
+          <div
+            class={`accordion ${listItem.toggled ? "toggled" : ""}`}
+            key={key}
+          >
+            <button
+              className="toggle"
+              onClick={() => handleAccordionToggle(listItem)}
+            >
+              <div className="labelcard">
+                <div className="number-highlighted">
                   <p>{listItem.index + 1}</p>
                 </div>
-                <p className='labelheadline'>{listItem.label}</p>
+                <p className="labelheadline">{listItem.label}</p>
               </div>
-              <div className='direction-indicator'>{listItem.toggled ? <MinusCircleFilled /> : <PlusCircleFilled />}</div>
+              <div className="direction-indicator">
+                {listItem.toggled ? (
+                  <MinusCircleFilled />
+                ) : (
+                  <PlusCircleFilled />
+                )}
+              </div>
             </button>
-            <div className='content-parent'>
-              <div className='content'>
-                <iframe src={listItem.video} width="784" height="441" frameborder="0" allowfullscreen="true"></iframe>
+            <div className="content-parent">
+              <div className="content">
+                <iframe
+                  src={listItem.video}
+                  width="784"
+                  height="441"
+                  frameborder="0"
+                  allowfullscreen="true"
+                ></iframe>
                 <div className="sub-content-head">
                   <label>Descriptions:</label>
                   <div className="sub-content-des">
-                      <p>{listItem.descriptions}</p>
+                    <p>{listItem.descriptions}</p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        )
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
 export default Accordion;
-
 
 // const Accordioncard = () => {
 //     useEffect(() => {
 //         const accordionContent = document.querySelectorAll(".accordion-content");
-  
+
 //         accordionContent.forEach((item, index) => {
 //         let header = item.querySelector("header");
 //         header.addEventListener("click", () => {
 //           item.classList.toggle("open");
-  
+
 //           let description = item.querySelector(".description")
 //           if (item.classList.contains("open")) {
 //             description.style.height = "50px";
@@ -95,7 +109,7 @@ export default Accordion;
 //           }
 //         });
 //       });
-  
+
 //       // Cleanup event listeners on component unmount
 //       return () => {
 //         accordionContent.forEach((item) => {
@@ -104,7 +118,7 @@ export default Accordion;
 //         });
 //       };
 //     }, []); // Empty dependency array means this effect runs once on mount
-  
+
 //     return (
 //         <div className="body">
 //         <div className="accordion">
